260a0b82d6d7d67ffe1a69420a41d8061b13453c
Rohan Goel <rohangoel0296@gmail.com>
2015-09-07 16:13:53 +0530

    added clicking on a link functionality

--- ReadFile.java
+++ ReadFile.java
@@ -9,7 +9,7 @@ import javax.swing.event.*;
 public class ReadFile extends JFrame{
 	
 	private JTextField addressBar;
-	private JEditorPane editorPane;
+	private JEditorPane display;
 	
 	public ReadFile(){
 		
@@ -25,12 +25,33 @@ public class ReadFile extends JFrame{
 					
 					@Override
 					public void actionPerformed(ActionEvent e) {
-						loadWebsite(e.getActionCommand());	//getActionCommand() returns the string inside the address bar associated with the hit enter event
+						//loadWebsite(e.getActionCommand());	//getActionCommand() returns the string inside the address bar associated with the hit enter event
 						
 					}
 				}	
 				);
-		add(addressBar,BorderLayout.NORTH);
-	}
+		add(addressBar,BorderLayout.NORTH);	//add the address bar and place it on the top of the screen
+		
+		display = new JEditorPane();
+		display.setEditable(false); 	//can't edit website
+		
+		display.addHyperlinkListener(
+				
+				/**
+				 * Handles when click on a hyperlink
+				 */
+				
+				new HyperlinkListener() {
+					
+					@Override
+					public void hyperlinkUpdate(HyperlinkEvent e) {
+						
+						if(e.getEventType()==HyperlinkEvent.EventType.ACTIVATED) // if click on the link
+							//loadWebsite(e.getURL().toString());	//convert the URL object to string (method loadWebsite takes string as an attribute)
+											
+					}
+				}
+				);
+			}
 
 }
